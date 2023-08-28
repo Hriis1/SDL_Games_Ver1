3b0901f6a128be82d632ee15cc6a72817dce1866
hrishd <hrishd@abv.bg>
2023-08-28 13:32:15 +0300

    pacman - added enum for direction in pacmandata

--- PacMan/PacManData.h
+++ PacMan/PacManData.h
@@ -3,3 +3,9 @@
 //Screen dimension constants
 const static int PACMAN_SCREEN_WIDTH = 1280;
 const static int PACMAN_SCREEN_HEIGHT = 720;
+
+//enum for directiom
+enum class DIRECTION
+{
+	LEFT = 0, RIGHT, UP, DPWN
+};
