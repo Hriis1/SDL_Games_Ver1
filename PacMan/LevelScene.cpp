e4d304ac7ed31a5e92736203815dacf48655d8cc
hrishd <hrishd@abv.bg>
2023-09-14 23:17:42 +0300

    pacman - added wall

--- PacMan/LevelScene.cpp
+++ PacMan/LevelScene.cpp
@@ -14,6 +14,7 @@ LevelScene::LevelScene(LWindow& win, bool& quitFlag)
     _walls.emplace_back(SDL_FRect{ 875, 260, 121, 15 });
 
     _walls.emplace_back(SDL_FRect{ 388, 275, 17, 44 });
+    _walls.emplace_back(SDL_FRect{ 875, 275, 17, 44 });
 
     _walls.emplace_back(SDL_FRect{ 284, 319, 121, 15 });
     _walls.emplace_back(SDL_FRect{ 875, 319, 121, 15 });
