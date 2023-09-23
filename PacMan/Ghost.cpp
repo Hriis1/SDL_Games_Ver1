6fdf91bd6a80b3b311b032903267c233ba9cfe55
hrishd <hrishd@abv.bg>
2023-09-23 23:01:58 +0300

    pacman - load ghost textures

--- PacMan/Ghost.cpp
+++ PacMan/Ghost.cpp
@@ -1,1 +1,97 @@
 #include "Ghost.h"
+
+const float Ghost::TEXTURE_SCALE = 2.0f;
+const float Ghost::Ghost_HEIGHT = 16 * Ghost::TEXTURE_SCALE;
+const float Ghost::Ghost_WIDTH = 16 * Ghost::TEXTURE_SCALE;
+LTexture Ghost::_redGhostSpriteSheet;
+LTexture Ghost::_greenGhostSpriteSheet;
+LTexture Ghost::_blueGhostSpriteSheet;
+LTexture Ghost::_orangeGhostSpriteSheet;
+LTexture Ghost::_yellowGhostSpriteSheet;
+SDL_Rect Ghost::_spriteClips[Ghost_ANIMATION_FRAMES];
+
+Ghost::Ghost(float xPos, float yPos, GhostType type)
+    : _xPos(xPos), _yPos(yPos), _type(type)
+{
+    shiftColliders();
+}
+
+bool Ghost::init(SDL_Renderer* renderer, SDL_Window* window)
+{
+    //init texture
+    _redGhostSpriteSheet.initRenderer(renderer);
+    _greenGhostSpriteSheet.initRenderer(renderer);
+    _blueGhostSpriteSheet.initRenderer(renderer);
+    _orangeGhostSpriteSheet.initRenderer(renderer);
+    _yellowGhostSpriteSheet.initRenderer(renderer);
+
+    if (!_redGhostSpriteSheet.loadFromFile("Assets/Textures/Coin.png", window))
+    {
+        printf("Could not load red ghost texture\n");
+        return false;
+    }
+    if (!_greenGhostSpriteSheet.loadFromFile("Assets/Textures/BigCoin.png", window))
+    {
+        printf("Could not load green ghost texture\n");
+        return false;
+    }
+    if (!_blueGhostSpriteSheet.loadFromFile("Assets/Textures/BigCoin.png", window))
+    {
+        printf("Could not load blue ghost texture\n");
+        return false;
+    }
+    if (!_orangeGhostSpriteSheet.loadFromFile("Assets/Textures/BigCoin.png", window))
+    {
+        printf("Could not load orange ghost texture\n");
+        return false;
+    }
+    if (!_yellowGhostSpriteSheet.loadFromFile("Assets/Textures/BigCoin.png", window))
+    {
+        printf("Could not load yellow ghost texture\n");
+        return false;
+    }
+
+    //init spriteclips
+    int textureWidth = Ghost_WIDTH / TEXTURE_SCALE;
+    int textureHeight = Ghost_HEIGHT / TEXTURE_SCALE;
+    _spriteClips[0] = { 0,0, textureWidth, textureHeight };
+    _spriteClips[1] = { textureWidth,0, textureWidth, textureHeight };
+    _spriteClips[2] = { textureWidth * 2,0, textureWidth, textureHeight };
+    _spriteClips[3] = { textureWidth * 3,0, textureWidth, textureHeight };
+    _spriteClips[4] = { textureWidth * 4,0, textureWidth, textureHeight };
+    _spriteClips[5] = { textureWidth * 5,0, textureWidth, textureHeight };
+    _spriteClips[6] = { textureWidth * 6,0, textureWidth, textureHeight };
+    _spriteClips[7] = { textureWidth * 7,0, textureWidth, textureHeight };
+
+    return true;
+}
+
+void Ghost::render(int camX, int camY)
+{
+    SDL_Rect* currentClip = &_spriteClips[_animationFrame / 12];
+
+    if (_type == GhostType::SMALL)
+    {
+        _smallGhostSpriteSheet.render(_xPos - camX, _yPos - camY, currentClip, TEXTURE_SCALE, 0.0, NULL, SDL_FLIP_NONE);
+    }
+    else //if the type is BIG
+    {
+        _bigGhostSpriteSheet.render(_xPos - camX, _yPos - camY, currentClip, TEXTURE_SCALE, 0.0, NULL, SDL_FLIP_NONE);
+    }
+
+    //Go to next frame
+    _animationFrame++;
+
+    //Cycle animation
+    if (_animationFrame / 12 >= Ghost_ANIMATION_FRAMES)
+    {
+        _animationFrame = 0;
+    }
+}
+
+void Ghost::shiftColliders()
+{
+    _collisionRect.x = _xPos;
+    _collisionRect.y = _yPos;
+}
+
