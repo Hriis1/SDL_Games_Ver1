2b5d12f11133be6807c45dd7cad6edcb5fb31941
hrishd <hrishd@abv.bg>
2023-12-24 00:15:15 +0200

    fix the scene order

--- PacMan/App.cpp
+++ PacMan/App.cpp
@@ -45,8 +45,8 @@ bool App::init()
 
 void App::initScenes()
 {
-    _scenes.push_back(std::make_unique<WinScene>(_window, _quit));
     _scenes.push_back(std::make_unique<LevelScene>(_window, _quit));
+    _scenes.push_back(std::make_unique<WinScene>(_window, _quit));
 }
 
 void App::run()
